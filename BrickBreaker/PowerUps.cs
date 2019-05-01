102f63a0a925cc700c70e2bf2b353c1dd4bc8c0b
Berkley Fair <berkley1515@gmail.com>
2019-05-01 09:56:39 -0400

    boi

--- BrickBreaker/PowerUps.cs
+++ BrickBreaker/PowerUps.cs
@@ -36,10 +36,7 @@ namespace BrickBreaker
             //use switch statement to check which powerup is colliding and do stuff
             switch (name)
             {
-                case "catchyBoi":
-
-                    break;
-
+                
                 case "multiBoi":
                     GameScreen.balls.Add(new Ball(GameScreen.balls[0].x + 10, GameScreen.balls[0].y, Math.Abs(GameScreen.balls[0].xSpeed), Math.Abs(GameScreen.balls[0].ySpeed), GameScreen.balls[0].size));
                     GameScreen.balls.Add(new Ball(GameScreen.balls[0].x - 10, GameScreen.balls[0].y, Math.Abs(GameScreen.balls[0].xSpeed), Math.Abs(GameScreen.balls[0].ySpeed), GameScreen.balls[0].size));
@@ -50,7 +47,7 @@ namespace BrickBreaker
                     break;
 
                 case "smallBoi":
-                    GameScreen.ChangePaddle(-20);
+                    GameScreen.ChangePaddle(-20); //
                     break;
 
                 case "enlargedBoi":
@@ -109,65 +106,7 @@ namespace BrickBreaker
         }
     }
 
-    //public class extraLife : PowerUps
-    //{
-        
-    //    public extraLife(int _x, int _y)
-    //    {
-    //        GameScreen.ChangeLives(1);
-
-    //    }
-
-    //}
-
-    //public class ballCatch
-    //{
-
-    //    public ballCatch(int _x, int _y)
-    //    {
-
-    //    }
-
-    //}
-    // public class multiBall
-    //{
-
-    //    public multiBall()
-    //    {
-
-    //    }
-           
-    //}
-
-
-    //public class paddleBig
-    //{
-
-    //    public paddleBig()
-    //    {
-    //        GameScreen.ChangePaddle(10); // stretch paddle by 10 pixels
-    //    }
-
-    //}
-
-    //public class paddleSmall
-    //{
-
-    //    public paddleSmall()
-    //    {
-    //        GameScreen.ChangePaddle(-10); // shrink paddle by 10 pixels
-    //    }
-
-    //}
-
-    //public class slowDown
-    //{
-
-    //    public slowDown()
-    //    {
-    //        GameScreen.ChangeSpeeds(-1, -1, -1);  // -1 for testing    (xSpeed, ySpeed, paddleSpeed)
-
-    //    }
-
-    //}
+   
+    
+    
 }
